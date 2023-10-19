7bc7b3b0282f22e81854ce5f8a16d2e6fe5193ff
Chinwuba Israel <chiizzy5@gmail.com>
2023-10-19 09:09:12 +0100

    more python code

---
+++ sayings.py
@@ -0,0 +1,11 @@
+def main():
+    hello('Izzy')
+    goodbye('Izzy')
+def hello(name):
+    print(f"Hello, {name}")
+
+def goodbye(name):
+    print(f"goodbye, {name}")
+    
+if __name__ == '__main__':
+    main()
